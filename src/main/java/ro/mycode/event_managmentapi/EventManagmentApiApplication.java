9d421eef2365a20386c145e679dbd7b8107036c4
kati1989 <kati_1989@yahoo.com>
2023-06-05 20:12:02 +0300

    First commit

---
+++ src/main/java/ro/mycode/event_managmentapi/EventManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.event_managmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/event_managmentapi/EventManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.event_managmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
